ea14816b79aeb55906b5518d52cd663cdbaddf9f
Yurie <yurie.koga.a@gmail.com>
2020-12-07 18:09:12 -0800

    Initial Commit

---
+++ Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment2
+//
+//  Created by Uji Saori on 2020-12-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
